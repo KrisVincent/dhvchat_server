616af4323174d4e340e2046bb08557355cf6d771
Pheytunn <87560373+Pheytunn@users.noreply.github.com>
2021-10-07 00:17:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Login(Resource):
-    def post(self):
-        print("User added")
-        print("bago")
-        return {'msg': 'User added successfully'}
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-api.add_resource(Login, '/login')
+class yameteKudasai(Resource):
+
+    def getgghg(self):
+        return "cheater"
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(yameteKudasai, '/hi')
 
 if __name__ == '__main__':
     app.run(debug=True)
