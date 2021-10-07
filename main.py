70473a907b2cc524a2f7274d3e90ed46de25cf73
John Dempsey <john.dempsey@csuci.edu>
2021-10-07 13:58:16 +0800

    added print block

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class Login(Resource):
+    def post(self):
+        print("User added")
+        return {'msg': 'User added successfully'}
 
-api.add_resource(HelloWorld, '/')
+
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
